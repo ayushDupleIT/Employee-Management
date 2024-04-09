38561d45ac3c00592327e0bd68930838c841a970
Ayush Duple <ayush.duple@gmail.com>
2024-04-09 17:39:43 +0530

    PDF

--- src/app/ApiRoutes.tsx
+++ src/app/ApiRoutes.tsx
@@ -1,10 +1,10 @@
-const JOB_URL = "http://localhost:8080/job/";
-const CANDIDATE_URL = "http://localhost:8080/candidate/";
-const CAT_URL = "http://localhost:8080/cat/";
+// const JOB_URL = "http://localhost:8080/job/";
+// const CANDIDATE_URL = "http://localhost:8080/candidate/";
+// const CAT_URL = "http://localhost:8080/cat/";
 
-// const JOB_URL = "http://64.227.173.23:8080/job/";
-// const CANDIDATE_URL = "http://64.227.173.23:8080/candidate/";
-// const CAT_URL = "http://64.227.173.23:8080/cat/";
+const JOB_URL = "http://64.227.173.23:8080/job/";
+const CANDIDATE_URL = "http://64.227.173.23:8080/candidate/";
+const CAT_URL = "http://64.227.173.23:8080/cat/";
 const AWS_S3 = "http://64.227.173.23:8080/aws-s3/image-upload/";
 
 const API = {
